6c43ab20b73bbf4e35818498e04723be92dd7333
Ellie King <ellie.king@digital.cabinet-office.gov.uk>
2017-10-31 10:50:25 +0000

    Try removeing test PWD as it is confusing for path in AWS

--- exp_1.sh
+++ exp_1.sh
@@ -2,7 +2,6 @@
 
 docker run -i --rm -v ${PWD}/output:/mnt/output \
     -v ${PWD}/experiments:/mnt/experiments \
-    -v ${PWD}/tests/input:/mnt/input \
     ukgovdatascience/govuk-lda-tagger-image:latest python train_lda.py \
     --output-topics /mnt/output/topics.csv \
     --output-tags /mnt/output/tags.csv \
